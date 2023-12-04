5995f3b85fb31723c9d6fb1113b1a8ad2d17bfdb
Ivan Mikurov <ivancad33@gmail.com>
2023-12-04 16:55:15 +0300

    tf6.2 add v2

--- 6.4tf2/src/outputs.tf
+++ 6.4tf2/src/outputs.tf
@@ -4,3 +4,5 @@ output "vm_ip_map" {
     "yandex_compute_instance.vm_db.name" = yandex_compute_instance.vm_db.network_interface.0.nat_ip_address
   }
 }
+
+output "admin_info" { value = "${local.test_map.admin} is admin for ${local.test_list[length(local.test_list)-1]} server based on OS ${local.servers[local.test_list[length(local.test_list)-1]]["image"]} with ${local.servers[local.test_list[length(local.test_list)-1]]["cpu"]} vcpu, ${local.servers[local.test_list[length(local.test_list)-1]]["ram"]} ram, and ${local.servers.production["disks"][0]}, ${local.servers.production["disks"][1]}, ${local.servers.production["disks"][2]}, ${local.servers.production["disks"][3]} virtual disks" }
