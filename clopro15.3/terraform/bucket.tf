d6e99ecfb1800d5ee21bd833b9a80800dc4e1bdc
Ivan Mikurov <ivancad33@gmail.com>
2024-08-06 21:55:03 +0300

    v1

---
+++ clopro15.3/terraform/bucket.tf
@@ -0,0 +1,57 @@
+locals {
+    current_timestamp = timestamp()
+    formatted_date = formatdate("DD-MM-YYYY", local.current_timestamp)
+    bucket_name = "mii-${local.formatted_date}"
+}
+
+// Создаем сервисный аккаунт для backet
+resource "yandex_iam_service_account" "service" {
+  folder_id = var.folder_id
+  name      = "bucket-sa"
+}
+
+// Назначение роли сервисному аккаунту
+resource "yandex_resourcemanager_folder_iam_member" "bucket-editor" {
+  folder_id = var.folder_id
+  role      = "storage.editor"
+  member    = "serviceAccount:${yandex_iam_service_account.service.id}"
+  depends_on = [yandex_iam_service_account.service]
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "sa-editor-encrypter-decrypter" {
+  folder_id = var.folder_id
+  role      = "kms.keys.encrypterDecrypter"
+  member    = "serviceAccount:${yandex_iam_service_account.service.id}"
+}
+
+// Создание статического ключа доступа
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.sa.id
+  description        = "static access key for object storage"
+}
+
+// Создание бакета с использованием ключа
+resource "yandex_storage_bucket" "mii" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket = local.bucket_name
+  acl    = "public-read-write"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = yandex_kms_symmetric_key.secret-key.id
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+resource "yandex_kms_symmetric_key" "secret-key" {
+  name              = var.kms_key_name
+  description       = var.kms_key_description
+  default_algorithm = var.default_algorithm
+  lifecycle {
+    prevent_destroy = var.prevent_destroy
+  }
+}
